5e632cb731a34bd4259ab5f5bae6da529826eca4
Glaymar <glaymar2010@live.com>
2020-12-12 03:30:33 -0300

    feat: implementa a validação das rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate')
 
 const AppointmentsControler = require('./controllers/AppointmentsControler');
 
@@ -7,14 +8,42 @@ const routes = express.Router();
 
 //lista todos os “appointments”
 routes.get('/Appointments', AppointmentsControler.list);
+
 //adiciona um novo “appointment”
-routes.post('/Appointments', AppointmentsControler.create);
+routes.post('/Appointments', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        id: Joi.string().required().regex(/appointment/),
+        address: Joi.string().required(),
+        patientName: Joi.string().required(),
+        dateTime: Joi.string().required(),
+        state: Joi.string().required()
+    })
+}), AppointmentsControler.create);
+
 //atualiza o state de um appointment
-routes.put('/Appointments/state', AppointmentsControler.update);
+routes.put('/Appointments/state', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        state: Joi.string().required()
+    }),
+    [Segments.HEADERS]: Joi.object({
+        id: Joi.string().required().regex(/appointment/),
+    }).unknown(),
+
+}), AppointmentsControler.update);
+
 //deleta um “appointment” com id específico da lista
-routes.delete('/Appointments/:id', AppointmentsControler.delete);
+routes.delete('/Appointments/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.string().required().regex(/appointment/)
+    })
+}), AppointmentsControler.delete);
+
 //listar appointment expecifico
-routes.get('/Appointmentselect/:id', AppointmentsControler.select);
+routes.get('/Appointmentselect/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.string().required().regex(/appointment/)
+    })
+}), AppointmentsControler.select);
 
 
 module.exports = routes;
