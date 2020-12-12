b115b301b8f451c0241e9eb18b5463ef223bc2da
Glaymar <glaymar2010@live.com>
2020-12-12 02:46:19 -0300

    feat: Implementa validações no AppointmentControler

---
+++ README.md
@@ -0,0 +1,36 @@
+# Projeto Agenda 
+
+## Bancos de Dados
+
+### FireBase
+1. será necessario criar uma conta (free) no Firebase.
+
+2. Crie um novo projeto.
+
+3. add um app web a esse projeto.
+
+4.Pegue a variavel de configuração. 
+
+
+
+
+## Executando Backend
+
+1. instalar dependencias do projeto
+    ```
+    npm install 
+    ```
+2. Criar arquivo .env na raiz do projeto, e preencher o seguinte modelo
+    ```
+    API_KEY = 
+    AUTH_DOMAIN=
+    DATABASE_URL= 
+    PROJECT_ID= 
+    STORAGE_BUCKET= 
+    APP_ID= 
+
+    ``` 
+5. Executar o servidor do projeto
+    ```
+    npm start
+    ```
--- backend/src/controllers/AppointmentsControler.js
+++ backend/src/controllers/AppointmentsControler.js
@@ -20,12 +20,13 @@ module.exports = {
         const result = await connection.ref('appointments/' + id)
             .once('value')
             .then((snapshot) => {
-                return snapshot.val();
+                const result = snapshot.val();
 
+                if (!result) {
+                    return response.status(400).json({ error: 'Appointment não existe' });
+                }
+                return response.json(result);
             });
-
-        return response.json(result);
-
     },
 
     async create(request, response) {
@@ -36,21 +37,50 @@ module.exports = {
             dateTime,
             state
         }
-        //Criação de um Appointments   
-        await connection.ref('appointments/' + id).set(dados)
-
-        return response.json("cadrasto");
+        //verifica se o appointment ja existe
+        await connection.ref('appointments/' + id)
+            .once('value')
+            .then((snapshot) => {
+                const respos = snapshot.val();
+                if (!respos) {
+                    //Criação de um Appointments   
+                    connection.ref('appointments/' + id).set(dados);
+                    return response.json("cadastrado");
+                } else {
+                    return response.status(400).json('appointment já cadastrado');
+                }
+            });
 
     },
 
     async update(request, response) {
         const id = request.headers.id;
-        const { state } = request.body
-        // atualizar o State
-        await connection.ref('appointments/' + id).update({ state })
+        const { state } = request.body;
 
+        //verifica se o appointment existe
+        const result = await connection.ref('appointments/' + id)
+            .once('value')
+            .then((snapshot) => {
+                const result = snapshot.val();
+                if (!result) {
+                    return response.status(400).json({ error: 'Appointment não existe' });
+                }  //verificar se o state existe
+                connection.ref('options/').once('value')
+                    .then((snapshot) => {
+                        const options = snapshot.val();
 
-        return response.json('State alterado com Sucesso');
+                        for (let index = 0; index < options.length; index++) {
+                            if (state == options[index]) {
+                                // atualizar o State
+                                connection.ref('appointments/' + id).update({ state });
+                                return response.json('State alterado com Sucesso');
+                            }
+                        }
+                        return response.status(400).json({ error: 'State não alterado, state n existe' });
+                    });
+
+            });
+
     },
 
     async delete(request, response) {
